cffc757feb6e0265220cb3b3d39fb091945093c4
Thien Pham <thienpham@Thiens-MacBook-Air.local>
2023-09-16 12:23:39 +1000

    add admin module

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('tavolaitaliano', 'root', 'Thapdienmaiphuc241!', {
-    host: 'localhost',
+const sequelize = new Sequelize('tavolai_italiano', 'theo', 'Thapdienmaiphuc241!', {
+    host: '34.172.65.146',
     dialect: 'mysql'
 });
 
---
+++ src/app/components/admin/menu-editing/menu-editing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuEditingComponent } from './menu-editing.component';
+
+
+
+@NgModule({
+  declarations: [
+    MenuEditingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MenuEditingModule { }
